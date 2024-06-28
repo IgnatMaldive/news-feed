2e1a5d71055cd25f2d491d98faba1862497e4621
Your Name <you@example.com>
2024-06-28 17:03:51 +0200

    vari

--- api/index.py
+++ api/index.py
@@ -1,18 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return 'Hello, world'
+def index():
+    query = request.args.get('query', 'Technology')  # Default query
+    api_key = '15e592a655654890885142a17dd3f6c9'  # Use your NewsAPI key
+    url = f'https://newsapi.org/v2/everything?q={query}&sortBy=publishedAt&apiKey={api_key}'
+    response = requests.get(url)
+    data = response.json()
+    articles = data.get('articles', [])
+    return render_template('index.html', articles=articles)
 
-
-@app.route('/test')
-def test():
-    return 'Test'
-
-@app.route('/result')
-def result():
-   dict = {'phy':50,'che':60,'maths':70}
-   return render_template('result.html', result = dict)
+if __name__ == '__main__':
+    app.run(debug=True)
